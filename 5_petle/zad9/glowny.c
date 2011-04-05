9fb060071907c5d3064fa5b554ad24f41b878981
kmieszala <mkonrad14@gmail.com>
2011-04-05 16:38:41 +0200

    poprawka

--- 5_petle/zad9/glowny.c
+++ 5_petle/zad9/glowny.c
@@ -5,22 +5,8 @@ int main()
 {
     char z[100];
     czytaj();
-    do {
-
-	printf("\n d - drukuj\n");
-	printf("o - oblicz\n");
-
-
-
-	fgets(z, 100, stdin);
-	switch (z[0]) {
-	case 'd':
-	    drukuj();
-	    break;
-	case 'o':
-	    oblicz();
-	    break;
-	}
-    } while (z[0] != 'k');
-    return (0);
+    drukuj();
+    oblicz();
+//while (z[0] != 'k');
+return 0;
 }
